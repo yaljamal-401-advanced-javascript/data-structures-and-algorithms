7adc1b195965a3134b4dc19cef95348b64109cd4
yaljamal <yazan.aljamal1@gmail.com>
2020-05-18 02:35:55 +0300

    fix some issue

--- challenges/arrayReverse/__test__/array-reverse.test.js
+++ challenges/arrayReverse/__test__/array-reverse.test.js
@@ -1,11 +1,12 @@
 'use strict';
 
-const reverse = require('./array-reverse.js');
+const reverseArray = require('../lib/array-reverse.js');
 
 describe('reverse()', () => {
     it('can reverse', () => {
-        let reversed = reverse([1, 2, 3, 4, 5]);
-        expect([1, 2, 3, 4, 5]).not.toEqual(reversed);
-        expect(reverse(reversed)).toEqual([1, 2, 3, 4, 5]);
+        // let reversed = reverseArray([1, 2, 3, 4, 5]);
+        // expect([1, 2, 3, 4, 5]).not.toEqual(reversed);
+        // expect(reverseArray(reversed)).toEqual([1, 2, 3, 4, 5]);
+        expect(reverseArray([1, 2, 3, 4, 5, 6])).toEqual([6, 5, 4, 3, 2, 1]);
     });
 });
