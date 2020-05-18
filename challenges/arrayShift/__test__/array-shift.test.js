9e0313e4e1ccf51a907f0ba289afe234c9caeec3
yaljamal <yazan.aljamal1@gmail.com>
2020-05-19 00:06:16 +0300

    solve chal 2

---
+++ challenges/arrayShift/__test__/array-shift.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const shiftArray = require('../lib/array-shift.js');
+
+describe('insertShiftArray()', () => {
+    it('insertShiftArray', () => {
+        expect(shiftArray([1,2,3,4,5],7)).toEqual([1,2,3,7,4,5]);
+        expect(shiftArray([2,4,6,8],5)).toEqual([2,4,5,6,8]);
+        expect(shiftArray([4,8,15,23,42],16)).toEqual([4,8,15,16,23,42]);
+
+    });
+});
