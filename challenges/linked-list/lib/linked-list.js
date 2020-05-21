70bc66df26f9713d5fab6d3cb72a9259d6b7e057
yaljamal <yazan.aljamal1@gmail.com>
2020-05-22 01:11:55 +0300

    linked list

---
+++ challenges/linked-list/lib/linked-list.js
@@ -0,0 +1,37 @@
+'use strict';
+const Node = require('./node.js');
+class LinkedList{
+    constructor(){
+        this.head=null;
+    }
+    insert(value){
+        const node=new Node(value);
+        if(!this.head){
+            this.head=node;
+            return this;
+        }
+        let currentNode=this.head;
+        while(currentNode.next){
+            currentNode=currentNode.next;
+        }
+        currentNode.next=node;
+        return this;
+    }
+    includes(value){
+        let node=this.head;
+        while(node){
+            if(node.value===value){
+                return true;
+            }
+            node.next;
+        }
+        return false;
+    }
+    toString(){
+
+
+
+    }
+
+}
+module.exports=LinkedList;
