ee766c76966a300eb4bc5c6c6a6b2913ea926ad6
yaljamal <yazan.aljamal1@gmail.com>
2020-06-05 22:08:33 +0300

    fix the algorithem

--- challenges/linked-list/lib/linked-list.js
+++ challenges/linked-list/lib/linked-list.js
@@ -106,15 +106,29 @@ class LinkedList {
   mergeLists(list1,list2){
     if(list1 && list2){
       let list3=new LinkedList();
-      list3.head=list1.head;
-      let current=list3.head;
-      while(current.next){
-        current=current.next;
+      if(list1.head.value){
+        list3.append(list1.head.value);
       }
-      current= current.next;
+      if(list2.head.value){
+        list3.append(list2.head.value);
+      }
+      let current1=list1.head;
+      let current2=list2.head;
+      while(current1.next || current2.next){
+        if(current1.next){
+          current1=current1.next;
+          list3.append(current1.value);
+        }
+        if(current2.next){
+          current2=current2.next;
+          list3.append(current2.value);
+        }
+      }
       return list3;
-    }else
-      return 'enter another list ';
+
+    }else{
+      return 'plz enter tow linked list ';
+    }
   }
 }
 module.exports = LinkedList;
